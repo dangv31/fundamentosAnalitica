34596247af61b34807ea3ef148dd46c5e486c66e
Daniel Giraldo <dangiraldova@unal.edu.co>
2025-08-13 22:11:23 -0500

    Talleres restantes

---
+++ Talleres/PRE-27-prediccion-basica-series-de-tiempo-dangv31-main/tests/test_homework.py
@@ -0,0 +1,12 @@
+"""Autograding script."""
+
+import os
+
+
+def test_homework():
+    """Test"""
+
+    assert os.path.exists("files/output/metrics.csv")
+    assert os.path.exists("files/output/forecasts.csv")
+
+test_homework()
